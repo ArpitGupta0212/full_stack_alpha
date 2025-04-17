13c34893f13fe0b02719c3d5dde29d0fe08e246c
ArpitGupta0212 <arpitboss52525@gmail.com>
2025-04-17 12:20:08 +0530

    2

---
+++ 17_day/operator.js
@@ -0,0 +1,6 @@
+// order precedence
+// ()
+// ** in multiple power operator right to left
+// *,/,% in this multiple case left to right
+// +,- in this multiple case left to right
+// Nan = Not A Number
