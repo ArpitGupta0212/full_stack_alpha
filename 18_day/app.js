9e03cbc9e6f54bfc758ef5399e3c2be83265b25d
ArpitGupta0212 <arpitboss52525@gmail.com>
2025-08-20 22:49:50 +0530

    javaScriptBasics

---
+++ 18_day/app.js
@@ -0,0 +1,18 @@
+// console.log("Hello World");
+// let x=5;
+// let y=10;
+// let output=`total is ${x+y} rupee.`
+// console.log(output);
+// console.log("sum :",x+y);
+// 2
+// let str="apple";
+// if(str[0]==='a' && str.length>3){
+//     console.log("safe");
+// }else{
+//     console.log("unSafe");
+// }
+// 3
+let x=12;
+switch(x){
+    case 12:console.log(x);
+}
