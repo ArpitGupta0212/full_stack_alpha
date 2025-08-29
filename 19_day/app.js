27109f8635b4dd90f1ab9f27b69f786e94feb2a4
ArpitGupta0212 <arpitboss52525@gmail.com>
2025-08-29 18:40:39 +0530

    19_day_practise_complete

---
+++ 19_day/app.js
@@ -0,0 +1,26 @@
+// let msg="help!";
+// console.log(msg.trim().toUpperCase());
+// let str="apnacollege";
+// console.log(str.slice(4).replaceAll('l','t'));
+// console.log("hi");
+// let arr=["arpit",23];
+// console.log(arr[0][3]);
+// let start=["january","july","march","august"];
+// start[0]="july";
+// start[1]="june";
+// let final=start;
+// console.log(final);
+// let arr=["c","c++","html","javascript","python","java","c#","sql"];
+// arr.reverse();
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]=="javascript"){
+//         console.log(i);
+//     }
+// }
+// let arr=[['x',null,'O'],[null,'x',null],['O',null,'x']];
+//         console.log(arr);
+// let arr=[1,2,3,4];
+// let n=3;
+// for(let i=0;i<n;i++){
+//     console.log(arr.pop());
+// }
